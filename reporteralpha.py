f1c4c4670cd621474250f5383958d8853c635c0b
sammedgane <sammedgane143@gmail.com>
2026-04-14 02:48:39 +0530

    Update reporteralpha.py

--- reporteralpha.py
+++ reporteralpha.py
@@ -6,10 +6,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('BOT-TOKEN')
+bot = telebot.TeleBot('8286782440:AAHR3HFdBAAnR0YArEdkTPnCQkzz50Rn5vQ')
 
 # Admin user IDs
-admin_id = ["YOUR-UID"]
+admin_id = ["6713699335"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
